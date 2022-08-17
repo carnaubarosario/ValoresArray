75234eaa7cfcfac3a23a74326c9c6e4b9e7d71fb
carnaubarosario <31799642+carnaubarosario@users.noreply.github.com>
2022-08-17 16:48:46 -0300

    Add files via upload

---
+++ InserirValoresArray/InserirValoresArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InserirValoresArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = new int[2];
+            int i = 0;
+            Array[0] = Inserir();
+            Array[1] = Inserir();
+            Console.WriteLine("\n\nResultado: {0}{1}\n\n", Array[0], Array[1]);
+        }
+
+        static int Inserir()
+        {
+            Console.WriteLine("Digite um valor inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+            return n;
+        }
+    }
+}
